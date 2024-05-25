ce05b5169f36037117011267c4360294e8b84366
ApoorvMisra149 <apoorvm149@gmail.com>
2024-05-25 18:21:42 +0530

    Update 04_comparison.js

--- 01_basics/04_comparison.js
+++ 01_basics/04_comparison.js
@@ -5,12 +5,14 @@
 // console.log(2 != 1);
 
 
-// console.log("2" > 1);
-// console.log("02" > 1);
+// console.log("2" > 1); // true-it automatically converts string to a no. if comparison operator is there
+// console.log("02" > 1); //true
 
-console.log(null > 0);
-console.log(null == 0);
-console.log(null >= 0);
+console.log(null > 0); // false
+console.log(null == 0); // false
+console.log(null >= 0); // true
+//the reason is that equality check(==) & comparison > >= < <= work differently
+//Comparison converts null to a number, treating it as a 0 , while == doesn't convert
 
 console.log(undefined == 0);
 console.log(undefined > 0);
