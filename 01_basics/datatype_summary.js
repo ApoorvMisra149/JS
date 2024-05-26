eefc41f45b01ca435fcbb169f0e9719c06acab52
ApoorvMisra149 <apoorvm149@gmail.com>
2024-05-26 11:29:25 +0530

    Update datatype_summary.js

--- 01_basics/datatype_summary.js
+++ 01_basics/datatype_summary.js
@@ -1,1 +1,37 @@
+//  Primitive
 
+//  7 types : String, Number, Boolearn, null, undefined, Symbol, BigInt
+
+const score = 100
+const scoreValue = 100.3
+
+const isLoggedIn = false
+const outsideTemp = null
+let userEmail;
+
+const id = Symbol('123')
+const anotherId = Symbol('123') // Symbol use to assign unique values to a variable even if the same value is input to 2 variables.
+
+console.log(id === anotherId); //here it will going to return false as the datatype of both is symbol which means they have unique value even if we assign same value
+
+// const bigNumber = 3456543576654356754n //by giving n at the last means that it is a BigInt
+
+
+
+// Reference (Non primitive)
+
+// Array, Objects, Functions
+
+const heros = ["shaktiman", "naagraj", "doga"];
+let myObj = {
+    name: "hitesh",
+    age: 22,
+}
+
+const myFunction = function(){
+    console.log("Hello world");
+}
+
+console.log(typeof anotherId);
+
+// https://262.ecma-international.org/5.1/#sec-11.4.3
