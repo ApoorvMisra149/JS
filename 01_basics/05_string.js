fc17e4416b43f35a2e4e2a161d85c3859c1e4605
ApoorvMisra149 <apoorvm149@gmail.com>
2024-06-02 11:11:39 +0530

    Update 05_string.js

--- 01_basics/05_string.js
+++ 01_basics/05_string.js
@@ -3,9 +3,9 @@ const repoCount = 50
 
 // console.log(name + repoCount + " Value");
 
-console.log(`Hello my name is ${name} and my repo count is ${repoCount}`);
+console.log(`Hello my name is ${name} and my repo count is ${repoCount}`); //you can use this method also to print variables with a text
 
-const gameName = new String('hitesh-hc-com')
+const gameName = new String('hitesh-hc-com') //another method of creating a string
 
 // console.log(gameName[0]);
 // console.log(gameName.__proto__);
