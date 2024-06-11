b33e0cffa4c268b8989e6ab1dfa3563672904f1d
ApoorvMisra149 <apoorvm149@gmail.com>
2024-06-11 18:34:38 +0000

    array

--- 02_basics/01_array.js
+++ 02_basics/01_array.js
@@ -8,15 +8,15 @@ const myArr2 = new Array(1, 2, 3, 4)
 
 // Array methods
 
-// myArr.push(6)
+// myArr.push(6) //push value=6 at last in array
 // myArr.push(7)
-// myArr.pop()
+// myArr.pop() //pop out last value of array
 
-// myArr.unshift(9)
+// myArr.unshift(9) //add value 9 at 0th position & shift all other elemnts by 1
 // myArr.shift()
 
-// console.log(myArr.includes(9));
-// console.log(myArr.indexOf(3));
+// console.log(myArr.includes(9)); //true/false
+// console.log(myArr.indexOf(3)); 
 
 // const newArr = myArr.join()
 
