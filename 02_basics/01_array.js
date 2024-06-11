cf7d9e7ca53f27dacb6ca9c55f917362f8d82ef9
ApoorvMisra149 <apoorvm149@gmail.com>
2024-06-11 19:02:01 +0000

    array

--- 02_basics/01_array.js
+++ 02_basics/01_array.js
@@ -25,15 +25,17 @@ const myArr2 = new Array(1, 2, 3, 4)
 
 
 // slice, splice
+//slice- it slices the array, but the original array is not changed. The returned value of slice is an string type.
+//splice- it slices the array, & the new array value of original array  would be the unsliced value. Also, here the 
 
 console.log("A ", myArr);
 
-const myn1 = myArr.slice(1, 3)
+const myn1 = myArr.slice(1, 3)// it gives output from 1 to 2 & 3 is not included. Returned value is a string
 
 console.log(myn1);
-console.log("B ", myArr);
+console.log("B ", myArr); //no chnage in original array
 
 
-const myn2 = myArr.splice(1, 3)
-console.log("C ", myArr);
+const myn2 = myArr.splice(1, 3) //it gives output from 1 to 3. Returned value is a string
+console.log("C ", myArr); //original value of array is changed, now it doesn't include values b/w 1 to 3.
 console.log(myn2);
