b961b521a7e95e17685b7894f660bcbb19bc0946
ApoorvMisra149 <apoorvm149@gmail.com>
2024-06-23 07:59:48 +0000

    obb

---
+++ 02_basics/04_object.js
@@ -0,0 +1,81 @@
+// const tinderUser = new Object()
+const tinderUser = {}   //another method of creating object.This is called Singleton object
+
+tinderUser.id = "123abc"
+tinderUser.name = "Sammy"
+tinderUser.isLoggedIn = false
+
+// console.log(tinderUser);
+
+const regularUser = {
+    email: "some@gmail.com",
+    fullname: {
+        userfullname: {
+            firstname: "hitesh",
+            lastname: "choudhary" //we can create a object inside object
+        }
+    }
+}
+
+// console.log(regularUser.fullname.userfullname.firstname); //to print nested object values , do like this
+
+const obj1 = {1: "a", 2: "b"}
+const obj2 = {3: "a", 4: "b"}
+const obj4 = {5: "a", 6: "b"}
+
+// const obj3 = { obj1, obj2 } //don't use this to combine objects, as it merges obj2 at single indesx
+// const obj3 = Object.assign({}, obj1, obj2, obj4) //Object.assign(target,source)
+
+const obj3 = {...obj1, ...obj2}// best method to join objects
+// console.log(obj3);
+
+
+const users = [
+    {
+        id: 1,
+        email: "h@gmail.com"
+    },
+    {
+        id: 1,
+        email: "h@gmail.com"
+    },
+    {
+        id: 1,
+        email: "h@gmail.com"
+    },
+]
+
+users[1].email
+// console.log(tinderUser);
+
+// console.log(Object.keys(tinderUser));
+// console.log(Object.values(tinderUser));
+// console.log(Object.entries(tinderUser));
+
+// console.log(tinderUser.hasOwnProperty('isLoggedIn'));
+
+
+const course = {
+    coursename: "js in hindi",
+    price: "999",
+    courseInstructor: "hitesh"
+}
+
+// course.courseInstructor
+
+const {courseInstructor: instructor} = course
+
+// console.log(courseInstructor);
+console.log(instructor);
+
+// {
+//     "name": "hitesh",
+//     "coursename": "js in hindi",
+//     "price": "free"
+// }
+
+[
+    {},
+    {},
+    {}
+]
