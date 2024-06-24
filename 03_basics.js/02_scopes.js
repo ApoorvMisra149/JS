2771931273746951f2e9e59b79ebc7036a1bf671
ApoorvMisra149 <apoorvm149@gmail.com>
2024-06-24 19:14:04 +0000

    d

--- 03_basics.js/02_scopes.js
+++ 03_basics.js/02_scopes.js
@@ -1,6 +1,6 @@
 
 if (true) {
-    let a = 10 //it is called as a block scope or local scope
+    let a = 10 //it is called as a block scope or local scope.Now this value cannot be accessed outside this scope
     const b = 20
     //var c = 300
     // console.log("INNER: ", a);
@@ -16,6 +16,8 @@ if (true) {
 let a=100 //now this is global scope
 
 
+
+//if we create nested functions, then the parent variables of a fnc. can be accessed inside child functions but not vice-versa
 function one(){
     const username = "hitesh"
 
@@ -23,38 +25,38 @@ function one(){
         const website = "youtube"
         console.log(username);
     }
-    // console.log(website);
-
+    // console.log(website); //output: undefined
      two()
-
 }
-
 // one()
 
+
+//similarly for nested if-
 if (true) {
     const username = "hitesh"
     if (username === "hitesh") {
         const website = " youtube"
         // console.log(username + website);
     }
-    // console.log(website);
+    // console.log(website);// undefined
 }
 
-// console.log(username);
+// console.log(username); //undefined
 
 
 // ++++++++++++++++++ interesting ++++++++++++++++++
-
-
+//We can declare function in 2 ways:
+// 1.
 console.log(addone(5))
-
 function addone(num){
     return num + 1
 }
 
-
-
-addTwo(5)
-const addTwo = function(num){
+// 2.
+addTwo(5) //here it will going to give error as in this type of function declaration we are holding it in a variable also.So that varaiable should be defined first.Here It is defined in the next line so addTwo(5) should come after declaration of function. We should be careful with this type of function declaration.
+const addTwo = function(num){     //this declaration of function 
     return num + 2
 }
+//variables in JS are very powerful, we can store anything even functions & objects
+
+
